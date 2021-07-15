1a2a6f62d15be3dec3e8a18a398aa0ea04155826
Rakesh Kumar Jena <rjena025@hotmail.com>
2021-07-15 20:22:51 +0530

    second commit

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,24 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
 import './Cards.css'
 import { Card } from 'react-bootstrap'
-export default function Cards(props) {
+export default function Cards({ src, company_name, name, dob, id_no, blood_group }) {
+    const [company_names, setCompanyName] = useState(company_name)
+    let update = () => {
+        setCompanyName('Tesla, Inc.')
+    }
+    let reset = () => {
+        window.location.reload(false);
+    }
     return (
         <div>
-            <Card className='card my-5' >
-                <Card.Img className='img' variant="top" src={props.src} />
+            <Card className='card my-3' >
+                <Card.Img className='img' variant="top" src={src} />
                 <Card.Body className='text-light'>
-                    <Card.Title className='text-dark text-center text-uppercase text-decoration-underline'>{props.company_name}</Card.Title>
+                    <Card.Title className='text-dark text-center text-uppercase text-decoration-underline'>{company_names}</Card.Title>
                     <Card.Text>
-                        Name: {props.name}
+                        Name: {name}
                     </Card.Text>
                     <Card.Text>
-                        DOB: {props.dob}
+                        DOB: {dob}
                     </Card.Text>
                     <Card.Text>
-                        ID No: {props.id_no}
+                        ID No: {id_no}
                     </Card.Text><Card.Text>
-                        Blood Group: {props.blood_group}
+                        Blood Group: {blood_group}
                     </Card.Text>
+                    <button onClick={update} className='btn btn-sm btn-outline-dark'>Update</button>
+                    <button onClick={reset} className='btn btn-sm btn-outline-danger mx-2'>Reset</button>
                 </Card.Body>
 
             </Card>
